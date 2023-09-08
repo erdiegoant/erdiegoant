8d4e469e64917ecf383d6b91d02f48b0d38ff289
Diego Barrera <erdiegoant@gmail.com>
2023-09-08 11:10:32 -0500

    Refactor conditional url rendering in ProjectItem blade
    In livewire's ProjectItem blade, conditionals were refactored to check if a project url is present before rendering it. This is to avoid having a dead link if a project url does not exist. The project title now also falls back to a span element without a link if there is no project url.

--- resources/views/livewire/project-item.blade.php
+++ resources/views/livewire/project-item.blade.php
@@ -2,18 +2,30 @@
     @if(!is_null($image))
         <div class="drop-shadow-lg">
             <div class="overflow-hidden rounded-lg">
-                <a @if(!empty($project->url)) href="{{ $project->url }}" target="_blank" rel="noopener noreferrer" @endif>
+                @if(!empty($project->url))
+                    <a href="{{ $project->url }}" target="_blank" rel="noopener noreferrer">
+                        <div class="cursor-pointer transition duration-200 ease-in-out transform hover:scale-110 rounded-lg flex justify-center">
+                            {{ $image }}
+                        </div>
+                    </a>
+                @else
                     <div class="cursor-pointer transition duration-200 ease-in-out transform hover:scale-110 rounded-lg flex justify-center">
                         {{ $image }}
                     </div>
-                </a>
+                @endif
             </div>
         </div>
     @endif
 
-    <a href="{{ $project->url }}" target="_blank" rel="noopener noreferrer" class="pt-5 text-[22px] font-semibold text-brand-gray hover:text-purple-500 duration-300 transition block">
-        <h3>{{ $project->title }}</h3>
-    </a>
+    @if(!empty($project->url))
+        <a href="{{ $project->url }}" target="_blank" rel="noopener noreferrer" class="pt-5 text-[22px] font-semibold text-brand-gray hover:text-purple-500 duration-300 transition block">
+            <h3>{{ $project->title }}</h3>
+        </a>
+    @else
+        <span class="pt-5 text-[22px] font-semibold text-brand-gray block">
+                <h3>{{ $project->title }}</h3>
+            </span>
+    @endif
     <span class="leading-relaxed text-brand-gray mt-2 block">
         {{ $project->description }}
     </span>
